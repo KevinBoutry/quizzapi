f7885a6c97e81f5baf228561ff13fc711f854b4d
bidule <truc>
2023-12-15 18:29:43 +0100

    rebuilt docker and main

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import * as fs from 'fs';
 
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const path = require('path');
 
-  const configService = new ConfigService();
+  const basePath = path.resolve(__dirname, '../certs/live/kafel.fr');
+
+  const certPath = fs.readlinkSync(
+    path.resolve(__dirname, '../certs/live/kafel.fr/fullchain.pem'),
+  );
+  const keyPath = fs.readlinkSync(
+    path.resolve(__dirname, '../certs/live/kafel.fr/privkey.pem'),
+  );
 
   const httpsOptions = {
-    key: fs.readFileSync(configService.get('CERT_KEY_URL')),
-    cert: fs.readFileSync(configService.get('CERT_URL')),
-  };  
+    key: fs.readFileSync(path.resolve(basePath, keyPath)),
+    cert: fs.readFileSync(path.resolve(basePath, certPath)),
+  };
 
   const app = await NestFactory.create(AppModule, { httpsOptions });
- 
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
