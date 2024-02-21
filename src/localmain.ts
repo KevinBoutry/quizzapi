a1a0cddf7a6ee430e7a3a9409f296e8ef23b9799
bidule <truc>
2024-02-21 13:04:57 +0100

    change main.ts

---
+++ src/localmain.ts
@@ -0,0 +1,17 @@
+// import * as fs from 'fs';
+
+// import { AppModule } from './app.module';
+// import { NestFactory } from '@nestjs/core';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   // eslint-disable-next-line @typescript-eslint/no-var-requires
+
+//   const app = await NestFactory.create(AppModule);
+
+  
+//   app.enableCors();
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(3000);
+// }
+// bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,24 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const path = require('path');
 
-  const app = await NestFactory.create(AppModule);
+  const basePath = path.resolve(__dirname, '../certs/live/kafel.fr');
 
-  
+  const certPath = fs.readlinkSync(
+    path.resolve(__dirname, '../certs/live/kafel.fr/fullchain.pem'),
+  );
+  const keyPath = fs.readlinkSync(
+    path.resolve(__dirname, '../certs/live/kafel.fr/privkey.pem'),
+  );
+
+  const httpsOptions = {
+    key: fs.readFileSync(path.resolve(basePath, keyPath)),
+    cert: fs.readFileSync(path.resolve(basePath, certPath)),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
